d8e5c76b4c30dff314fbbbb443a2e275f2413797
kikiinfinit <adiepanggayuh@gmail.com>
2022-08-20 17:37:22 +0800

    Add Helper and constant

---
+++ src/constant/apiUrl.js
@@ -0,0 +1,9 @@
+const BASE_URL = 'https://pokeapi.co/api/v2/';
+const GET_POKEMON_LIST_URL = BASE_URL + 'pokemon';
+const GET_POKEMON_TYPES_URL = BASE_URL + 'type';
+
+export {
+    BASE_URL,
+    GET_POKEMON_LIST_URL,
+    GET_POKEMON_TYPES_URL
+}
---
+++ src/helper/helper.js
@@ -0,0 +1,42 @@
+import bg from '../constant/bgImages';
+import defaultImage from '../media/default.svg';
+const getBgImage = (poke) => {
+    let type = getFirstType(poke);
+    return type ? bg[type] : bg.normal;
+}
+const getFirstType = (poke) => {
+    if (poke.types && poke.types.length) {
+        return poke.types[0].type.name;
+    } else {
+        return null;
+    }
+}
+
+const getImage = (poke) => {
+    return poke?.sprites?.other?.dream_world?.front_default || defaultImage;
+}
+const extractValue = (array, paramName) => {
+    return array.reduce((acc, cur) => {
+        acc.push(cur[paramName].name);
+        return acc;
+    }, []);
+}
+
+const capitalizeFirstChar = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+const filterPokemonsByType = (data, types) => {
+    data = data.filter(item=> item.details.types.find(rec=>rec.type.name === types));
+    console.log('filtered', data);
+    return data;
+}
+
+export {
+    getBgImage,
+    getFirstType,
+    getImage,
+    extractValue,
+    capitalizeFirstChar,
+    filterPokemonsByType,
+}
